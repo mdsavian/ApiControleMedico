4d0a19dd6044911f7f64dfc05c07edb70b1c8559
Marlon <marlon.savian@gmail.com>
2019-02-06 14:11:39 -0200

    s

--- ApiControleMedico/Controllers/UploadController.cs
+++ ApiControleMedico/Controllers/UploadController.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Path = System.IO.Path;
 
 namespace ApiControleMedico.Controllers
 {
@@ -42,6 +47,8 @@ namespace ApiControleMedico.Controllers
                     {
                         file.CopyTo(stream);
                     }
+
+                    ProcessarArquivoPdf(fullPath);
                 }
                 return Json("Upload Successful.");
             }
@@ -50,6 +57,64 @@ namespace ApiControleMedico.Controllers
                 return Json("Upload Failed: " + ex.Message);
             }
         }
+
+
+        public void ProcessarArquivoPdf(string strPath)
+        {
+            using (PdfReader reader = new PdfReader(strPath))
+            {
+                StringBuilder text = new StringBuilder();
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    var nomePaciente = NomePaciente(reader);
+                    var tipoPlano = TipoPlano(reader);
+                    var servico = Servico(reader);
+                    var movimento = Movimento(reader);
+//                    i = 70;
+                }
+            }
+        }
+        private string Movimento(PdfReader reader, int pagina)
+        {
+            RenderFilter[] filters = new RenderFilter[1];
+            LocationTextExtractionStrategy regionFilter = new LocationTextExtractionStrategy();
+            filters[0] = new RegionTextRenderFilter(new Rectangle(85, 500, 600, 535));
+            FilteredTextRenderListener strategy = new FilteredTextRenderListener(regionFilter, filters);
+            string result = PdfTextExtractor.GetTextFromPage(reader, pagina, strategy);
+
+
+
+            return result;
+        }
+        private string Servico(PdfReader reader, int pagina)
+        {
+            RenderFilter[] filters = new RenderFilter[1];
+            LocationTextExtractionStrategy regionFilter = new LocationTextExtractionStrategy();
+            filters[0] = new RegionTextRenderFilter(new Rectangle(85, 600, 600, 645));
+            FilteredTextRenderListener strategy = new FilteredTextRenderListener(regionFilter, filters);
+            string result = PdfTextExtractor.GetTextFromPage(reader, pagina, strategy);
+            return result;
+        }
+        private string TipoPlano(PdfReader reader, int pagina)
+        {
+            RenderFilter[] filters = new RenderFilter[1];
+            LocationTextExtractionStrategy regionFilter = new LocationTextExtractionStrategy();
+            filters[0] = new RegionTextRenderFilter(new Rectangle(85, 400, 600, 425));
+            FilteredTextRenderListener strategy = new FilteredTextRenderListener(regionFilter, filters);
+            string result = PdfTextExtractor.GetTextFromPage(reader, pagina, strategy);
+            return result;
+        }
+        private string NomePaciente(PdfReader reader, int pagina)
+        {
+            RenderFilter[] filters = new RenderFilter[1];
+            LocationTextExtractionStrategy regionFilter = new LocationTextExtractionStrategy();
+            filters[0] = new RegionTextRenderFilter(new Rectangle(85, 300, 600, 315));
+            FilteredTextRenderListener strategy = new FilteredTextRenderListener(regionFilter, filters);
+            string result = PdfTextExtractor.GetTextFromPage(reader, pagina , strategy);
+            return result;
+        }
+    
 
 
     }
