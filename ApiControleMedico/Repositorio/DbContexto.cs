323d2516876b26dc0deb3ab32491ac4af8640436
marlon savian <marlon.savian@gmail.com>
2019-03-19 06:45:11 -0300

    s

--- ApiControleMedico/Repositorio/DbContexto.cs
+++ ApiControleMedico/Repositorio/DbContexto.cs
@@ -16,47 +16,47 @@ namespace ApiControleMedico.Repositorio
         public DbContexto(string collectionName)
         {
             //Conexão com produção
-            string connectionString =
-                @"mongodb://controlemedico:DJdM5g8hFGmg1CtqWVwSw2Vw7kkv83aVGK0LTwagzpagUftQCorpWh8URLpd1EFNASBj2gPBhSJ0oFCzioBkfg==@controlemedico.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
-            MongoClientSettings settings = MongoClientSettings.FromUrl(
-                new MongoUrl(connectionString)
-            );
+            //string connectionString =
+            //    @"mongodb://controlemedico:DJdM5g8hFGmg1CtqWVwSw2Vw7kkv83aVGK0LTwagzpagUftQCorpWh8URLpd1EFNASBj2gPBhSJ0oFCzioBkfg==@controlemedico.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+            //MongoClientSettings settings = MongoClientSettings.FromUrl(
+            //    new MongoUrl(connectionString)
+            //);
 
-            settings.SslSettings =
-                new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
-            var client = new MongoClient(settings);
+            //settings.SslSettings =
+            //    new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            //var client = new MongoClient(settings);
 
             //conexão debug
-            //var client = new MongoClient("mongodb://localhost:27017");
+            var client = new MongoClient("mongodb://localhost:27017");
             Database = client.GetDatabase("ControleMedicoDb");
             
             //Database.RunCommand({ shardCollection: "yourCollection", key: { rateId: "_id" } } )
 
-            RegisterMapIfNeeded<Paciente>();
-            RegisterMapIfNeeded<Pessoa>();
-            RegisterMapIfNeeded<Usuario>();
-            RegisterMapIfNeeded<Medico>();
-            RegisterMapIfNeeded<Convenio>();
-
-
-            var partition = new BsonDocument {
-                {"shardCollection", $"{Database.DatabaseNamespace.DatabaseName}.{collectionName}"},
-                {"key", new BsonDocument {{$"{collectionName}Id", "hashed"}}}
-            };
-            var command = new BsonDocumentCommand<BsonDocument>(partition);
-            Database.RunCommandAsync(command);
-
+            RegisterMapIfNeeded<Paciente>(collectionName);
+            RegisterMapIfNeeded<Pessoa>(collectionName);
+            RegisterMapIfNeeded<Usuario>(collectionName);
+            RegisterMapIfNeeded<Medico>(collectionName);
+            RegisterMapIfNeeded<Convenio>(collectionName);
+            
             Collection = Database.GetCollection<T>(collectionName);
         }
-
-       
+        
 
         // Check to see if map is registered before registering class map
         // This is for the sake of the polymorphic types that we are using so Mongo knows how to deserialize
-        public void RegisterMapIfNeeded<TClass>()
+        public void RegisterMapIfNeeded<TClass>(string collectionName)
         {
             if (!BsonClassMap.IsClassMapRegistered(typeof(TClass)))
+            {
                 BsonClassMap.RegisterClassMap<TClass>();
+
+                var partition = new BsonDocument {
+                    {"shardCollection", $"{Database.DatabaseNamespace.DatabaseName}.{collectionName}"},
+                    {"key", new BsonDocument {{$"{collectionName}Id", "hashed"}}}
+                };
+                var command = new BsonDocumentCommand<BsonDocument>(partition);
+                Database.RunCommandAsync(command);
+            }
         }
 
     }
