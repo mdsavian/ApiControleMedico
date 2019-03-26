a25bee225d1cd40840434961434d3992b6d04bc0
marlon savian <marlon.savian@gmail.com>
2019-03-26 06:52:02 -0300

    s

--- ApiControleMedico/Controllers/FuncionarioController.cs
+++ ApiControleMedico/Controllers/FuncionarioController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using ApiControleMedico.Modelos;
 using ApiControleMedico.Services;
 using Microsoft.AspNetCore.Mvc;
